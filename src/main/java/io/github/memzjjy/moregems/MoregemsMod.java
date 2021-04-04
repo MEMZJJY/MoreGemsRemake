50e999201c17ae698495cfb6addef7fac8cf9792
MEMZJJY <931583432@qq.com>
2021-04-04 19:14:16 +0800

    添加蓝宝石，物品组

--- src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
+++ src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
@@ -1,17 +1,38 @@
 package io.github.memzjjy.moregems;
 
+import io.github.memzjjy.moregems.item.BlueGemItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MoregemsMod implements ModInitializer {
-	public static final Item BLUE_GEM_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
-	@Override
-	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier("moregems", "blue_gem_item"), BLUE_GEM_ITEM);
-		System.out.println("Hello Fabric world!");
-	}
+    public static final BlueGemItem BLUE_GEM_ITEM = new BlueGemItem(new FabricItemSettings());//创建蓝宝石实例
+    public static final ItemGroup MOREGEMS_GROUP = FabricItemGroupBuilder.create(
+            new Identifier("moregems","moregems_group"))
+            .icon(() -> new ItemStack(BLUE_GEM_ITEM))
+            .appendItems(stacks ->{
+                stacks.add(new ItemStack(BLUE_GEM_ITEM));
+            })
+            .build();
+
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("moregems", "blue_gem_item"), BLUE_GEM_ITEM);//注册蓝宝石
+        System.out.println("\n       　  　▃▆█▇▄▖\n" +
+                "　 　 　 ▟◤▖　　　◥█▎\n" +
+                "   　 ◢◤　 ▐　　　 　▐▉\n" +
+                "　 ▗◤　　　▂　▗▖　　▕█▎\n" +
+                "　◤　▗▅▖◥▄　▀◣　　█▊\n" +
+                "▐　▕▎◥▖◣◤　　　　◢██\n" +
+                "█◣　◥▅█▀　　　　▐██◤\n" +
+                "▐█▙▂　　     　◢██◤\n" +
+                "◥██◣　　　　◢▄◤\n" +
+                " 　　▀██▅▇▀");
+    }
 }
---
+++ src/main/java/io/github/memzjjy/moregems/item/BlueGemItem.java
@@ -0,0 +1,9 @@
+package io.github.memzjjy.moregems.item;
+
+import net.minecraft.item.Item;
+
+public class BlueGemItem extends Item {
+    public BlueGemItem(Settings settings){
+        super(settings);
+    }
+}
